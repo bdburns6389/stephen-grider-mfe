52ae059b07ddc50093aa0b8c3acba4c48f5b5eb9
Brian Burns <bdburns6389@gmail.com>
2023-05-04 21:15:11 -0500

    Initial Commit

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Create the mounting point for all the code.
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+// If we are in dev mode and in isolation, call mount immediately.
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// If we are running through a container, we should export the mount function.
+export { mount };
