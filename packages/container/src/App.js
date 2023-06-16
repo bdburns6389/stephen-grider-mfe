107109bc160fee9ab2c8e186a813785578411685
Brian Burns <bdburns6389@gmail.com>
2023-06-16 08:28:14 -0500

    Changed name

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hello There All!!!</h1>
+      <h1>Hello There Brian!!!</h1>
       <hr />
       <MarketingApp />
     </div>
